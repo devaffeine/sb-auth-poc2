fcb95b25b9ba5bbf532ccf127d1f92a124b53018
gilberto <gilberto.vento@gmail.com>
2023-04-17 15:48:58 -0400

    Initial import

---
+++ src/main/java/com/devaffeine/auth2/SbAuthPoc2Application.java
@@ -0,0 +1,13 @@
+package com.devaffeine.auth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbAuthPoc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbAuthPoc2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/devaffeine/auth2/SbAuthPoc2ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.devaffeine.auth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class SbAuthPoc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
