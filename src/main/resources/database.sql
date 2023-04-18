e34fae5ae576e3f5c840b7a65c41483e7fdca6b7
gilberto <gilberto.vento@gmail.com>
2023-04-18 12:05:22 -0400

    Initial import

--- src/main/resources/database.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS tb_users (
-    id INT AUTO_INCREMENT,
-    username VARCHAR(255),
-    password VARCHAR(512),
-    PRIMARY KEY(id)
-);
-
-ALTER TABLE tb_users ADD INDEX (username);
--- tests/exporter.sql
+++ tests/exporter.sql
@@ -1,2 +1,11 @@
 CREATE USER 'exporter'@'%' IDENTIFIED BY 'exporterpassword' WITH MAX_USER_CONNECTIONS 3;
 GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO 'exporter'@'%';
+
+CREATE TABLE IF NOT EXISTS tb_users (
+                                        id INT AUTO_INCREMENT,
+                                        username VARCHAR(255),
+    password VARCHAR(512),
+    PRIMARY KEY(id)
+    );
+
+ALTER TABLE tb_users ADD INDEX (username);
